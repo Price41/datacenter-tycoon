18b9fd69677b19353dfe5ce724e7be8047915b00
a-antoine <price41-@live.fr>
2016-08-01 21:15:25 +0200

    Server buying : OK

---
+++ src/AppBundle/Controller/ServersController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Server;
+
+class ServersController extends Controller
+{
+    /**
+     * @Route("/servers", name="servers")
+     */
+    public function indexAction()
+    {
+        $user = $this->getUser();
+        $session = $this->get('session');
+        $session->set('user_id', $user->getId());
+        $session->set('username', $user->getUsername());
+
+        $em = $this->getDoctrine()->getManager();
+        $datacenters = $em->getRepository('AppBundle:Datacenter')->findByPlayer($user);
+        $typeServers = $em->getRepository('AppBundle:TypeServer')->findAll();
+
+        return $this->render('servers/index.html.twig', array(
+            'datacenters' => $datacenters,
+            'type_servers' => $typeServers
+        ));
+    }
+
+    /**
+     * @Route("/servers/buy/{name}", name="servers_buy")
+     */
+    public function buyAction($name)
+    {
+        $user = $this->getUser();
+        $session = $this->get('session');
+        $session->set('user_id', $user->getId());
+        $session->set('username', $user->getUsername());
+
+        $em = $this->getDoctrine()->getManager();
+        $datacenters = $em->getRepository('AppBundle:Datacenter')->findByPlayer($user);
+        $typeServer = $em->getRepository('AppBundle:TypeServer')->findOneByName($name);
+
+
+        // Check if player have enough money
+        if($user->getBalance() >= $typeServer->getBuyingCost()) {
+
+
+            // Check if there is enough empty space in a rack
+            // TODO not yet implemented
+
+            $rack = $datacenters[0]->getRacks()[0]; // get the 1st rack in the 1st datacenter
+
+            $server = new Server();
+            $server->setTypeServer($typeServer);
+            $server->setUsageCpu(0);
+            $server->setUsageRam(0);
+            $server->setUsageHdd(0);
+            $server->setUsageLan(0);
+            $server->setUsageWan(0);
+            $server->setRack($rack);
+            $em->persist($server);
+
+            $user->setBalance($user->getBalance() - $typeServer->getBuyingCost());
+            $em->flush();
+
+        }
+
+        return $this->redirectToRoute('dashboard');
+    }
+}
